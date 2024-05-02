7aecb3fc5a7b575d7e22d5297355e35d4ec587fc
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-02 02:31:41 -0300

    criando o corpo da função Menu

--- main.dart
+++ main.dart
@@ -1,3 +1,17 @@
+import 'dart:io';
 void main(List<String> args){
   
+}
+
+void menu(){
+  print('Digite [1] para somar');
+  print('Digite [2] para subtrair');
+  print('Digite [3] para multiplicar');
+  print('Digite [4] para dividir');
+  print('Digite [5] para sair !!');
+  String? opEntrada = stdin.readLineSync();
+  print('Digite o primeiro valor');
+  String? entradaValor1 = stdin.readLineSync();
+  print('Digite o segundo valor');
+  String? entradaValor2 = stdin.readLineSync();
 }
