7bf3a2a0478b6dc36a697a44be5c4ef174380e99
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-02 03:55:40 -0300

    adicionando uma interface

---
+++ interface_calculadora.dart
@@ -0,0 +1,7 @@
+abstract class InterfaceCalculadora{
+  double soma({required double valo1r,required double valor2});
+  double subtracao({required double valor1,required double valor2});
+  double multiplicacao({required double valor1,required double valor2});
+  double divisao({required double valor1,required double valor2});
+
+}
