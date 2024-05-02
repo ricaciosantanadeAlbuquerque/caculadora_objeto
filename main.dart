1dcae56e5e18de0ba6c1404237367f9188950b4a
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-02 04:20:49 -0300

    função soma

--- main.dart
+++ main.dart
@@ -2,10 +2,11 @@ import 'dart:io';
 
 import 'calculadora.dart';
 void main(List<String> args){
-  menu();
+ var calculadora = menu();
+ print(calculadora.somaValor);
 }
 
-void menu(){
+Calculadora menu(){
   print('Digite [1] para somar');
   print('Digite [2] para subtrair');
   print('Digite [3] para multiplicar');
@@ -25,8 +26,8 @@ void menu(){
        double valor1 = double.tryParse(entradaValor1) ?? 0.0;
        double valor2 =  double.tryParse(entradaValor2) ?? 0.0;
 
-       Calculadora(op: op, valor1: valor1, valor2: valor2); // linha de instanciamento
-       
+      var calculadora =  Calculadora(op: op, valor1: valor1, valor2: valor2); // linha de instanciamento
+      return calculadora;
       }catch(e){
         throw Exception('ERRO! $opEntrada e $entradaValor1 e $entradaValor2 não são valores numéricos');
         }
